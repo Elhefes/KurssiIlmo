1dfa2d6a4a97c597c7526a357cee871968b3bd28
henripalin <henri.j.palin@gmail.com>
2019-10-06 17:58:42 +0300

    paranneltu validointia

--- application/courses/forms.py
+++ application/courses/forms.py
@@ -1,15 +1,24 @@
 from flask_wtf import FlaskForm
-from wtforms import BooleanField, StringField, validators, DateField, TextAreaField
+from wtforms import BooleanField, StringField, validators, DateField, TextAreaField, ValidationError
 from datetime import datetime
 
 class CourseForm(FlaskForm):
-    name = StringField("Kurssin nimi", [validators.Length(min=2, max=15)])
-    location = StringField("Paikka", [validators.Length(min=2, max=15)])
+    name = StringField("Kurssin nimi", [validators.Length(min=2, max=15, message='Kurssin nimen täytyy olla pituudeltaan 2-15 merkkiä!')])
+    location = StringField("Paikka", [validators.Length(min=2, max=15, message='Paikan nimen täytyy olla pituudeltaan 2-15 merkkiä!')])
     startingDate = DateField('Alku', [validators.InputRequired(message='Kurssin aika')], default=datetime.now())
     endingDate = DateField('Loppu', [validators.InputRequired(message='Kurssin aika')], default=datetime.now())
-    description = TextAreaField('Kuvaus', [validators.Length(min=2, max=300)])
+    description = TextAreaField('Kuvaus', [validators.Length(min=2, max=300, message='Kuvauksen täytyy olla pituudeltaan 2-300 merkkiä!')])
     price = StringField('Hinta', default = "0")
-    enroll = BooleanField("Done")
+    
+    def validate_startingDate(form, field):
+        if field.data > form.endingDate.data:
+            raise ValidationError('Kurssin loppupäivämäärä ei voi olla ennen alkamispäivämäärää')
+
+    def validate_price(form, field):
+        try:
+            float(field.data)
+        except ValueError:
+            raise ValidationError('Hinta täytyy olla numeerinen luku. Erota pisteellä eurot ja sentit.')
 
     class Meta:
         csrf = False
